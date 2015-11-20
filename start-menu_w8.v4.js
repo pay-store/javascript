026fa4b1df073baf17f1ed7e8497ab30d75209bf
pay-store <putrabagussetyawan@gmail.com>
2015-11-20 16:39:19 +0700

    Update start-menu_w8.v4.js

--- start-menu_w8.v4.js
+++ start-menu_w8.v4.js
@@ -9,7 +9,7 @@ document.write('<div class="mta_bar bar_logo" style="position:absolute;top:30%;"
 +'<a href="#buku"><img class="star" src="http://4.bp.blogspot.com/-DMLt0Y95elE/T1Ru07ScmlI/AAAAAAAABUQ/sD5wx12l1xc/s1600/mydocument.png" title="Chat"/></a><br/>'
 +'<a id="start-baru"><img class="star" src="http://4.bp.blogspot.com/-e8EqPj15VGM/T0DMmN7MT7I/AAAAAAAABMs/bd11bBMQSMw/s1600/Start+Button4.png" title="Click here to begin"/></a><br/>'
 +'<a href="#profil-saya"><img class="star" src="http://2.bp.blogspot.com/-GfcQ8QCp42Q/UGVtNNmlZkI/AAAAAAAAAuo/2LtCHBYZYDU/s1600/profil-saya+lah.png" title="Profil Saya"/></a><br/>'
-+'<a href="#irvan-credit"><img class="star" src="http://img1.blogblog.com/img/icon18_wrench_allbkg.png" title="Re-Created By Mazbeib" width="28"/></a>'
++'<a href="#pay-store-credit"><img class="star" src="http://img1.blogblog.com/img/icon18_wrench_allbkg.png" title="Re-Created By Mazbeib" width="28"/></a>'
 +'</center>');
 document.write('</div>');
 /* ]]> */

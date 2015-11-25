38df9fbc5f22a79ea675ff64e1cf1930b516e14b
pay-store <putrabagussetyawan@gmail.com>
2015-11-25 19:45:30 +0700

    Update jantung-irvan_w8.v4.js

--- jantung-irvan_w8.v4.js
+++ jantung-irvan_w8.v4.js
@@ -8,10 +8,10 @@ document.write('<div id="da-vinci-carousel">'
 +'<a class="mindah-jantung" href="'+''+Url_twitter_anda+''+'" title="Twitter"><img class="cloudcarousel" src="http://4.bp.blogspot.com/-TKzTJCcq1ic/URNrtVUgTSI/AAAAAAAAB_o/8r6TJ6iQx2c/s150/twitter-windows-sm.jpg"/></a>'
 +'<a class="mindah-jantung" href="'+''+Url_facebook_anda+''+'" title="Facebook"><img class="cloudcarousel" src="http://1.bp.blogspot.com/-y8KCj3YcgSw/URNrnEDrLLI/AAAAAAAAB_A/J-mqO1tuTvE/s150/facebook-windows-sm.jpg"/></a>'
 +'<a class="mindah-jantung" href="#profil-saya" title="Profil Saya"><img class="cloudcarousel" src="http://1.bp.blogspot.com/-T0aPgEdF8Kc/URNrsLeEEtI/AAAAAAAAB_g/bNjBhlH0G3g/s150/profil-windows-sm.jpg"/></a>'
-+'<a class="mindah-jantung" href="http://bl4ckcodes.blogspot.com" title="Bl4ckcodes Te4m"><img class="cloudcarousel" src="http://irvanefendyblogger.googlecode.com/files/bct-150x100.png"/></a>'
++'<a class="mindah-jantung" href="http://healthnfashion247.blogspot.co.id" title="Your Style"><img class="cloudcarousel" src="http://3.bp.blogspot.com/-PCoVKRVXHAM/VlWcZpzYSSI/AAAAAAAAAWw/2UiDbuMuld4/s1600/200061_tokoh-animasi-hatsune_663_498.jpg"/></a>'
 +'<a class="mindah-jantung" href="#buku" title="Chat"><img class="cloudcarousel" src="http://2.bp.blogspot.com/-SoFacoFf0hM/URNrt_WiMJI/AAAAAAAAB_w/Cd4q_hrE3M4/s150/chat-windows-sm.jpg"/></a>'
 +'<a class="mindah-jantung" href="http://www.blogger.com/follow-blog.g?blogID='+''+ID_blog_anda+''+'" title="Ikuti Blog ini"><img class="cloudcarousel" src="http://2.bp.blogspot.com/-_95uqov6JGs/URNrovg3k1I/AAAAAAAAB_I/CCD44Iw1QL4/s150/follow-windows-sm.jpg"/></a>'
-+'<a class="mindah-jantung" href="'+''+Url_animasi_foto_anda+''+'" title="Foto Saya"><img class="cloudcarousel" src="http://1.bp.blogspot.com/-BCwRXlonncI/URNrqYnqDoI/AAAAAAAAB_Y/TlMfTW78a5s/s150/photo-windows-sm.jpg"/></a>'
++'<a class="mindah-jantung" href="'+''+Url_animasi_foto_anda+''+'" title="Bukti dan Testi"><img class="cloudcarousel" src="http://1.bp.blogspot.com/-BCwRXlonncI/URNrqYnqDoI/AAAAAAAAB_Y/TlMfTW78a5s/s150/photo-windows-sm.jpg"/></a>'
 +'<a class="mindah-jantung" href="'+''+Url_sitemap_anda+''+'" title="Site Map"><img class="cloudcarousel" src="http://1.bp.blogspot.com/-5kN2EeFzbwM/URNrt-1XrFI/AAAAAAAAB_0/qVeNZlypcR8/s150/sitemap-windows-sm.jpg"/></a>');
 document.write('</div>');
 /* ]]> */
